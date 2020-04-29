48c0d6095af0c15a52f61b38700bc7e8d5adedcf
CodeXaver <mitchel.okomor@gmail.com>
2020-04-29 10:51:17 +0100

    added google meta tag

--- pages/components/Head.js
+++ pages/components/Head.js
@@ -4,9 +4,10 @@ import NextHead from 'next/head'
 const Head = ({ }) => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>Petros Group</title>
-    <meta name="description" content= 'Petros Group is a leading company which deals with wide range of products, including petroleum, oil, gas and construction engineering.' />
+    <title>Petros Group</title> 
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content= 'Petros Group is a leading company which deals with wide range of products, including petroleum, oil, gas and construction engineering.' />
+    <meta name="google-site-verification" content="VZoxisDaBP9D2IwdVx2B4NYvTFZpOAB8OoSpdIGWK10" />
     <link rel="icon" href="favicon.jpg" sizes="16x16" type="image/png" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Zilla+Slab:400,700" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
